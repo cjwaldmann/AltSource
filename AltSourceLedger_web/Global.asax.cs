af01c90e6fd909260bde026865c007cc451df1b8
cjwaldmann <32178491+cjwaldmann@users.noreply.github.com>
2017-09-22 09:17:32 -0400

    Add files via upload

---
+++ AltSourceLedger_web/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using AltSourceLedger_Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace AltSourceLedger_Web
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+            AltSourceCache.InitializeCache();
+
+        }
+
+        protected void Session_Start()
+        {
+            Session["CurrentUser"] = null;
+        }
+    }
+}
