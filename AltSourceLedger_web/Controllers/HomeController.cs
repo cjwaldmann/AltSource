253d3dba58050baa77ef45f90c5d72e3502f3d9d
cjwaldmann <32178491+cjwaldmann@users.noreply.github.com>
2017-09-22 09:26:51 -0400

    Add files via upload

---
+++ AltSourceLedger_web/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using AltSourceLedger_Objects;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+//*********************************
+//Author: Waldmann
+//Purpos: Main account interface page
+//**********************************
+namespace AltSourceLedger_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "AltSource Ledger - Account Details";
+            //ensure the user is logged in
+            if (Session["CurrentUser"] == null)
+            {
+                return RedirectToAction("Index", "Login");
+
+            }
+            return View();
+        }
+
+        public ActionResult GetAccount()
+        {
+            //ensure the user is logged in
+            if (Session["CurrentAccount"] == null)
+            {
+                //throw eror leting the interface know to redirect to login
+                return new HttpStatusCodeResult(502, "Your session has expired."); // Unauthorized
+            }
+
+            //convert the account listing to a json representation and
+            //send tot he interface
+            var ji = JsonConvert.SerializeObject((BankAccount) Session["CurrentAccount"]);
+            return Content(ji, "application/json");
+
+        }
+
+
+        [HttpPost]
+        public ActionResult ExecuteTransaction(string transaction)
+        {
+            if (Session["CurrentAccount"] == null)
+            {
+                //throw eror leting the interface know to redirect to login
+                return new HttpStatusCodeResult(502, "Your session has expired."); // Unauthorized
+            }
+
+            //translate the passed json arguement into an anonymous type for processing
+            var transDev = new {  amount = 0.00,  transactiontype = "" };
+            var vTrans = JsonConvert.DeserializeAnonymousType(transaction, transDev);
+
+            switch (vTrans.transactiontype)
+            {
+                case "d":
+                    //deposit
+                    ((BankAccount)Session["CurrentAccount"]).MakeDeposit(Convert.ToSingle(vTrans.amount), Session["CurrentUser"].ToString());
+                    break;
+                case "w":
+                    //if the account has sufficient funds, withdraw, otherwise throw an error
+                    if (((BankAccount)Session["CurrentAccount"]).CurrentBalance >= vTrans.amount)
+                    {
+                        ((BankAccount)Session["CurrentAccount"]).MakeWithdrawal(Convert.ToSingle(vTrans.amount), Session["CurrentUser"].ToString());
+                    }else
+                    {
+                        return new HttpStatusCodeResult(501, "This amount exceeds the current available balance."); // Unauthorized
+
+                    }
+                    break;
+            }
+
+            //convert the  updated account listing to a json representation and
+            //send tot he interface
+            var ji = JsonConvert.SerializeObject((BankAccount)Session["CurrentAccount"]);
+
+            return Content(ji, "application/json");
+
+        }
+    }
+}
---
+++ AltSourceLedger_web/Controllers/LoginController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using AltSourceLedger_Objects;
+
+//*********************************
+//Author: Waldmann
+//Purpos: Login/Account Creation screen
+//**********************************
+namespace AltSourceLedger_Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            ViewBag.Title = "AltSource Ledger - Login";
+
+            //clear any lingering session variables to log out the user
+            Session["CurrentAccount"] = null;
+            Session["CurrentUser"] = null;
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult RegisterUser(string userInfo)
+        {
+            //convert the arguement into an anonymous type for processing
+            var userDef = new { userName = "", passWord = "" };
+            var user = JsonConvert.DeserializeAnonymousType(userInfo, userDef);
+
+            string result;
+
+            List<AltSourceUser> users = (List<AltSourceUser>)AltSourceCache.Cache["users"];
+            //check to see if user already exists
+            if (users.FirstOrDefault<AltSourceUser>(u => u.username.Equals(user.userName, StringComparison.InvariantCultureIgnoreCase)) == null)
+            {
+                //user doesn't exist create and return success message
+                AltSourceUser newuser = AltSourceUser.CreateUser(user.userName, user.passWord);
+                ((List<AltSourceUser>)AltSourceCache.Cache["users"]).Add(newuser);
+
+                //create a new bank account for the new user
+                BankAccount acct = BankAccount.NewAccount(newuser.username);
+                ((List<BankAccount>)AltSourceCache.Cache["accounts"]).Add(acct);
+
+                result = "SUCCESS";
+            }
+            else
+            {
+                //let interface know of duplicate account isssue
+                result = "DUPLICATE";
+
+            }
+
+            return Content(result, "text/plain");
+        }
+
+
+        [HttpPost]
+        public ActionResult LoginUser(string userInfo)
+        {
+            //convert the arguement into an anonymous type for processing            List<AltSourceUser> users = (List<AltSourceUser>)AltSourceCache.Cache["users"];
+            var userDef = new { userName = "", passWord = "" };
+            var vUser = JsonConvert.DeserializeAnonymousType(userInfo, userDef);
+
+            string result;
+
+            List<AltSourceUser> users = (List<AltSourceUser>)AltSourceCache.Cache["users"];
+            AltSourceUser user = users.FirstOrDefault<AltSourceUser>(u => u.username.Equals(vUser.userName, StringComparison.InvariantCultureIgnoreCase) && u.password.Equals(vUser.passWord));
+            //make sure the user exists and that the password is correct
+            if (user != null)
+            {
+                //everything checks out, 
+                result = "SUCCESS";
+                Session["currentUser"] = user.username;
+
+                //make sure that the user has an account (they should, but good to check
+                BankAccount account;
+                account = ((List<BankAccount>)AltSourceCache.Cache["accounts"]).FirstOrDefault<BankAccount>(a => a.AccountOwner.Equals(user.username, StringComparison.InvariantCultureIgnoreCase));
+                if (account == null)
+                {
+                    account = BankAccount.NewAccount(user.username);
+                    ((List<BankAccount>)AltSourceCache.Cache["accounts"]).Add(account);
+
+                }
+
+                Session["CurrentAccount"] = account; 
+            }
+            else
+            {
+                //notify interface of login failure
+                result = "FAILURE"; ;
+            }
+
+            return Content(result, "text/plain");
+        }
+    }
+}
